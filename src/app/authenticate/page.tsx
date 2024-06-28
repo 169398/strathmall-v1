6e6a1592ebb3f7a22e03ede2f1e1b841138f9eab
idris <kulubiidris@gmail.com>
2024-06-28 22:13:27 +0300

    removed google oauth login for later

--- src/app/authenticate/page.tsx
+++ src/app/authenticate/page.tsx
@@ -14,17 +14,15 @@ const  AuthenticatePage =    async ()=> {
     redirect('/dashboard')
   }
   return (
-    <div className='relative flex w-full h-screen bg-background'>
-        <div className='max-w-3xl absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
-
-        <GoogleOAuthButton/>
-        <div className='h-4'></div>
-<TabSwitcher SignInTab={<SignInForm/>} SignUpTab={<SignUpForm/>} />
-
-
-        </div>
+    <div className="relative flex w-full h-screen bg-background">
+      <div className="max-w-3xl absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      {/* Commented out the google oauth button ,I will fix later*/}
+        {/* <GoogleOAuthButton/> */}
+        <div className="h-4"></div>
+        <TabSwitcher SignInTab={<SignInForm />} SignUpTab={<SignUpForm />} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default AuthenticatePage
